198db4e159a44d8d3a1237ad836737185836215a
mnsekh111 <mnsekh111@gmail.com>
2016-09-10 01:06:33 -0400

    poker hand test

--- code/3/Poker/PokerHand.py
+++ code/3/Poker/PokerHand.py
@@ -7,9 +7,17 @@ License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
 """
 
 from Card import *
+import copy
 
 
 class PokerHand(Hand):
+    def rank_card_mapper(self):
+        self.rank_card_map = {}
+        for ind in range(1, 14):
+            self.rank_card_map[ind] = []
+
+        for card in self.cards:
+            self.rank_card_map.get(card.rank).append(card)
 
     def rank_hist(self):
         """
@@ -19,7 +27,6 @@ class PokerHand(Hand):
         self.ranks = {}
         for card in self.cards:
             self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
-
 
     def suit_hist(self):
         """Builds a histogram of the suits that appear in the hand.
@@ -40,7 +47,7 @@ class PokerHand(Hand):
                 return True
         return False
 
-    def has_nkind(self,n):
+    def has_nkind(self, n):
         self.rank_hist();
         for val in self.ranks.values():
             if val >= n:
@@ -65,16 +72,105 @@ class PokerHand(Hand):
         return self.has_nkind(3)
 
     def has_straight(self):
-        return
+        self.rank_hist()
+        print self.ranks
+
+        for iind in range(1, 11):
+            found = True
+
+            if iind != 10:
+                for jind in range(0, 5):
+                    if self.ranks.get(iind + jind) == None:
+                        found = False
+
+                if found:
+                    # return True
+                    print found, range(iind, iind + jind + 1)
+                else:
+                    iind += (jind + 1)
+            else:
+                for jind in range(0, 4):
+                    if self.ranks.get(iind + jind) == None:
+                        found = False
+
+                if self.ranks.get(1) == None:
+                    found = False
+
+                if found:
+                    # return True
+                    print found, range(iind, iind + jind + 1)
+                else:
+                    iind += (jind + 1)
+
+        return False;
 
     def has_fullhouse(self):
-        return
+        return self.has_nkind(2) and self.has_nkind(3)
 
     def has_fourkind(self):
         return self.has_nkind(4)
 
+    def belong_to_same_suit(self, rank_list, suit):
+        self.rank_card_mapper()
+        found = True
+        for ind in range(0, len(rank_list)):
+            curr_rank = rank_list[ind]
+            curr_rank_cards = self.rank_card_map.get(curr_rank)
+
+            curr_rank_found = False
+            for card in curr_rank_cards:
+                if card.suit == suit:
+                    curr_rank_found = True
+                    break
+
+            if not curr_rank_found:
+                return False
+        return found
+
     def has_straightflush(self):
-        return
+        self.rank_hist()
+
+        for iind in range(1, 11):
+            found = True
+
+            if iind != 10:
+                for jind in range(0, 5):
+                    if self.ranks.get(iind + jind) == None:
+                        found = False
+
+                if found:
+                    rank_list = range(iind, jind + iind + 1)
+                    for suit in range(0, len(Card.suit_names)):
+                        if self.belong_to_same_suit(rank_list,suit):
+                            return True
+
+                    return False
+
+                # print found, iind, iind + jind
+                else:
+                    iind += (jind + 1)
+            else:
+                for jind in range(0, 4):
+                    if self.ranks.get(iind + jind) == None:
+                        found = False
+
+                if self.ranks.get(1) == None:
+                    found = False
+
+                if found:
+                    rank_list = range(iind, jind + iind + 1)
+                    rank_list.append(1)
+
+                    for suit in range(0, len(Card.suit_names)):
+                        if self.belong_to_same_suit(rank_list,suit):
+                            return True
+
+                    return False
+
+                else:
+                    iind += (jind + 1)
+
+        return False;
 
 
 if __name__ == '__main__':
@@ -82,12 +178,21 @@ if __name__ == '__main__':
     deck = Deck()
     deck.shuffle()
 
-    # deal the cards and classify the hands
+    # hand = PokerHand()
+    # deck.move_cards(hand, 10)
+    # hand.sort()
+    # print hand.has_straight()
+    # print hand.has_straightflush()
+
+    deal the cards and classify the hands
     for i in range(7):
         hand = PokerHand()
         deck.move_cards(hand, 7)
         hand.sort()
         print hand
-        print hand.has_flush()
+        print "Flush :" + str(hand.has_flush())
+        print "Four kind :" + str(hand.has_fourkind())
+        print "Pair : " + str(hand.has_pair())
+        print "Two Pair : " + str(hand.has_twopair())
+        print "Three kind : " + str(hand.has_threekind())
         print ''
-
