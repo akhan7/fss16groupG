131d8cc9a5f5db1e84afcc9fcd3d705e6a76f9c4
Ahmad Saad Khan <ahmadsaadkhan@Ahmads-MacBook-Pro.local>
2016-09-09 21:35:32 -0400

    Added bday paradox 2 code

---
+++ code/3/BirthdayParadox_2.py
@@ -0,0 +1,28 @@
+import random
+from collections import Counter
+
+def has_duplicate(lst):
+	collectlst = Counter(lst) 
+	for i in collectlst:
+		if collectlst[i] > 1:
+			#print 'Duplicate items found'
+			return True
+		else:
+			#print 'Duplicate items not found'
+			return False
+
+def assign_bday():
+	birthday = []
+	for i in xrange(23):
+		bday = random.randint(1, 365)
+		birthday.append(bday)
+	return birthday
+
+count, sample = 0, 1000
+for i in range(sample):
+	birthday = assign_bday()
+	if has_duplicate(birthday) == True:
+		count += 1
+
+print 'After %d simulations:' % sample
+print '%d simulations with at least one match' % count
