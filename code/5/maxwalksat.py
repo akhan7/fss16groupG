3294efe2be4956597bf0b8c0c442fd652930f941
mnsekh111 <mnsekh111@gmail.com>
2016-10-02 02:31:15 -0400

    added threshold

--- code/5/maxwalksat.py
+++ code/5/maxwalksat.py
@@ -3,20 +3,10 @@ import random
 import math
 
 
-def r(a=0, b=1): return random.random()
+def r(a=0, b=1): return random.randint(a, b)
 
 
 class Osyczka:
-    def f1(self, x):
-        return -25 * pow(x[0] - 2, 2) - pow(x[1] - 2, 2) \
-               - pow(x[2] - 1, 2) - pow(x[3] - 4, 2) - pow(x[4] - 1, 2)
-
-    def f2(self, x):
-        sum = 0
-        for i in xrange(len(x)):
-            sum = sum + pow(x[i], 2)
-        return sum
-
     def min_max(self, tries):
         i = 0
         min = float('inf')
@@ -34,18 +24,39 @@ class Osyczka:
 
         return min, max
 
-    def get_normalized(self, x, min, max):
-        return ((self.f1(x) + self.f2(x)) - min) / (max - min)
+    def get_threshold(self, g_min, g_max, tries):
+        i = 0
+        aggregate_diff = 0
+        while i < tries:
+            min, max = self.min_max(1000)
+            if min > g_min and max < g_max:
+                aggregate_diff += self.get_normalized(min, g_min, g_max)
+                i += 1
+
+        return aggregate_diff / float(tries)
+
+    def get_normalized(self, x, g_min, g_max):
+        return ((x) - g_min) / float(g_max - g_min)
 
     def osyczka(self, x):
-        return self.f1(x) + self.f2(x)
+        return self.__f1(x) + self.__f2(x)
 
     def constraints(self, x):
         return self.__g1(x) >= 0 and self.__g2(x) >= 0 and self.__g3(x) >= 0 and self.__g4(x) >= 0 \
                and self.__g5(x) >= 0 and self.__g6(x) >= 0
 
     def rand_tuple(self):
-        return (r(0, 10), r(0, 10), r(1, 5), r(0, 6), r(1, 5))
+        return (r(0, 10), r(0, 10), r(1, 5), r(0, 6), r(1, 5), r(0, 10))
+
+    def __f1(self, x):
+        return -25 * pow(x[0] - 2, 2) - pow(x[1] - 2, 2) \
+               - pow(x[2] - 1, 2) - pow(x[3] - 4, 2) - pow(x[4] - 1, 2)
+
+    def __f2(self, x):
+        sum = 0
+        for i in xrange(len(x)):
+            sum = sum + pow(x[i], 2)
+        return sum
 
     def __g1(self, x):
         return x[0] + x[1] - 2
@@ -73,6 +84,7 @@ class MaxWalkSat:
 
 if __name__ == '__main__':
     o = Osyczka()
-    print(o.f1((1, 2, 3, 4, 5, 6)))
-    print(o.f2((1, 2, 3, 4, 5, 6)))
-    print(o.constraints((0, 1, 3, 4, 5, 6)))
+    min, max = o.min_max(5000)
+
+    print(min, max)
+    print(o.get_threshold(min, max, 10))
