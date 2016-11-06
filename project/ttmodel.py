1ca30dc15359c291aa54d3567a3c87a15120a8b7
mnsekh111 <mnsekh111@gmail.com>
2016-11-06 14:36:23 -0500

    rename files

--- project/ttmodel.py
+++ project/ttmodel.py
@@ -3,10 +3,10 @@ from model import Model
 from decision import Decision
 
 
-class TimeTableSched(Model):
+class TimeTableModel(Model):
     def __init__(self):
         objectives = [self.__f1, self.__f2]
-        decisions = [Decision("x1", -5, 5), Decision("x2", -5, 5), Decision("x3", -5, 5)]
+        decisions = []
         constraints = []
         Model.__init__(self, decisions, objectives,constraints)
 
